19680a55ff211346bd179a3d340d914f5fad5e47
muzammilpro <muzammilhusnain7@gmail.com>
2023-12-10 20:16:49 +0500

    Under Procedure

--- app/components/loginnSignup.js
+++ app/components/loginnSignup.js
@@ -1,23 +1,9 @@
-import { useSession, signIn, signOut } from "next-auth/react"
+import React from 'react'
 
 export default function loginnSignup() {
- <>
-
-{/* const { data: session } = useSession()
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
-  }
   return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
-  */}
- </>
+    <div>
+      
+    </div>
   )
 }
